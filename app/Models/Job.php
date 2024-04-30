84b58d53b13d6c60c10650e669ddf405a7f65f38
Postmind <53375202+max-menshenin@users.noreply.github.com>
2024-04-30 19:31:52 +0300

    Lesson 11 finished

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,7 +7,12 @@ use \Illuminate\Database\Eloquent\Factories\HasFactory, \Illuminate\Notification
 
 class Job extends Model {
     use HasFactory;
+
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
 }
