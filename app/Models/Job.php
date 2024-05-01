bd176b4212fb19f2d9cf663a7b438e850612b4f2
Postmind <53375202+max-menshenin@users.noreply.github.com>
2024-05-01 18:12:51 +0300

    Lesson 12 bug

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -15,4 +15,9 @@ class Job extends Model {
         return $this->belongsTo(Employer::class);
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany(Job::class, foreignPivotKey: "job_listings");
+    }
+
 }
